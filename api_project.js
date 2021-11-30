c1010256bf3ec0163b61cb6f02c031369ae860b4
王磊 <wl_znzx@163.com>
2021-11-30 14:26:43 +0800

    first commit

---
+++ api_project.js
@@ -0,0 +1,24 @@
+define({
+  "name": "比亚迪放电工装OTA接口文档",
+  "version": "1.0.0",
+  "description": "放电工装在线升级API",
+  "title": "title",
+  "url": "https://47.98.58.40:8089/iot",
+  "order": [
+    "GetUser",
+    "PostUser"
+  ],
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-11-30T06:20:26.530Z",
+    "url": "https://apidocjs.com",
+    "version": "0.26.0"
+  }
+});
